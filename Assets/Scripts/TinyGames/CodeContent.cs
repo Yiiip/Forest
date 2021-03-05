448ff17c4a39456bca59ecc02668b4e54a3289bf
regen <regen@coconutislandstudio.com>
2021-03-05 20:01:16 +0800

    tiny game coding

---
+++ Assets/Scripts/TinyGames/CodeContent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CodeContent : MonoBehaviour
+{
+    Texture2D textureCodeContent;
+    GameObject selections;
+    int id;
+    TinyGame_Coding gameRef;
+    Button button;
+    void Awake()
+    {
+        selections = transform.Find("Selections").gameObject;
+        button = GetComponent<Button>();
+    }
+    void Start()
+    {
+        HideSelections();
+    }
+
+    public CodeContent Init(TinyGame_Coding refe, int id)
+    {
+        this.id = id;
+        gameRef = refe;
+        button.onClick.AddListener(() => gameRef.OnAnswerSelect(id));
+        return this;
+    }
+
+    public void ShowSelections()
+    {
+        selections.SetActive(true);
+    }
+
+    public void HideSelections()
+    {
+        selections.SetActive(false);
+    }
+
+    public void OnButtonClicked(int i)
+    {
+        gameRef.OnAnswerSelected(id, i);
+    }
+}
---
+++ Assets/Scripts/TinyGames/TinyGame_Coding.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeContentType = TinyGame_Coding_Helper.CodeContentType;
+public class TinyGame_Coding : MonoBehaviour
+{
+    
+    public Texture2D[] codeContentTextures;
+    public CodeContentType[] answers;
+    public CodeContentType[] requirements;
+    public GameObject codeContentPrefab;
+    private CodeContent[] contents;
+    void Awake()
+    {
+        var contentTypes = System.Enum.GetNames(typeof(CodeContentType));
+        codeContentTextures = new Texture2D[contentTypes.Length];
+        for (int i = 0; i < contentTypes.Length; i++)
+        {
+            codeContentTextures[i] = Resources.Load<Texture2D>(contentTypes[i]);
+        }
+    }
+
+    void Start()
+    {  
+        GenerateNewIssue(4, false, false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateNewIssue(int codeLength, bool containsIf, bool taskWillChange)
+    {
+        answers = new CodeContentType[codeLength];
+        for (int i = 0; i < codeLength; i++)
+        {
+            answers[i] = (CodeContentType)(-1);
+        }
+        requirements = TinyGame_Coding_Helper.GenerateRequirement(codeLength);
+        contents = new CodeContent[codeLength];
+        for (int i = 0; i < codeLength; i++)
+        {
+            contents[i] = Instantiate(codeContentPrefab).GetComponent<CodeContent>().Init(this, i);
+            contents[i].transform.SetParent(transform);
+            contents[i].transform.position += Vector3.right * i * 100;
+        }
+    }
+
+    public void OnAnswerSelect(int contentIndex)
+    {
+        foreach (var item in contents)
+        {
+            item.HideSelections();
+        }
+        contents[contentIndex].ShowSelections();
+    }
+
+    public void OnAnswerSelected(int contentIndex, int selectionIndex)
+    {
+        Debug.Log($"contentIndex = {contentIndex}");
+        Debug.Log($"selectionIndex = {selectionIndex}");
+        contents[contentIndex].HideSelections();
+        if (contentIndex < requirements.Length - 1)
+        {
+            contents[contentIndex + 1].ShowSelections();
+        }
+
+    }
+
+    public void OnDoneButtonClick()
+    {
+        TinyGame_Coding_Helper.CheckResult(requirements, answers);
+    }
+
+
+
+}
+
+public static class TinyGame_Coding_Helper
+{
+    public enum CodeContentType
+    {
+        Circle = 0,
+        Square = 1,
+        Triangle = 2,
+    }
+    public static CodeContentType[] GenerateRequirement(int codeLength)
+    {
+        var ret = new CodeContentType[codeLength];
+        
+        for (int i = 0; i < codeLength; i++)
+        {
+            ret[i] = (CodeContentType)Random.Range(0, codeLength - 1);
+        }
+        return ret;
+    }
+
+    public static int CheckResult(CodeContentType[] requirements, CodeContentType[] answers)
+    {
+        int correctAnswer = 0;
+        for (int i = 0; i < requirements.Length; i++)
+        {
+            if (requirements[i] == answers[i])
+            {
+                correctAnswer++;
+            }
+        }
+        return correctAnswer;
+    }
+}
