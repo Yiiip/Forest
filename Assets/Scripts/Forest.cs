82daf04cd3ae90168f7b9917e6e0d835a5896222
Felix <984082450@qq.com>
2021-03-05 19:07:49 +0800

    草的图片

---
+++ Assets/Scripts/Forest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forest : MonoBehaviour
+{
+    public float GlobalTimer;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GlobalTimer += 1;
+    }
+}
