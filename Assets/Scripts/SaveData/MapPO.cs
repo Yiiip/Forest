2aec665bc53e105907a092cc2c03fa0b0a4baa88
Felix <984082450@qq.com>
2021-03-12 19:29:52 +0800

    MapPO

---
+++ Assets/Scripts/SaveData/MapPO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MapPO : IPersistentObject
+{
+    public void Init()
+    {
+    }
+}
